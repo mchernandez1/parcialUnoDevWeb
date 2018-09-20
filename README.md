61a67a679eb4762be93930d2b023694af173a265
Maria Camila Hernandez <mc.hernandez1@uniandes.edu.co>
2018-09-20 09:28:17 -0500

    Commit # 2

---
+++ README.md
@@ -0,0 +1,14 @@
+# Desarrollo web examen 1 - 2018-2
+
+Este es el proyecto como resultado de la primera parte del curso de desarollo WEB.
+
+
+## Intalación
+
+### Back-end:
+El back-end del proyecto esta corriendo en el puerto 3001. Para poder abrir el back es necesario por medio de la consola del sistema operativo navegar hasta la carpeta donde se encuentra el proyecto y ejecutar el comando yarn add, este comando instalará todas las dependencias necesarias para correr el proyecto.
+Posteriormente, se ejecuta el comando yarn start para poder correr el archivo. 
+
+### Back-end más Front-end
+El front corre en el puerto 3000. Para empezar la aplicación es necesario navegar sobre la carpeta del proyecto hasta la carpeta front y ejecutar los mismos comandos descritos anteriormente al navegar hasta la carpeta front del proyecto.
+
---
+++ routes/index.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+});
+
+/* Find All documents*/
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs);
+    callback(docs);
+  });
+}
+
+/* Remove a document */
+const removeDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Delete document where a is 3
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });
+}
+
+/* Update a document */ 
+const updateDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });
+}
+
+module.exports = router;
